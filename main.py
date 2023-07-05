5ad4a02819d699af78029a7e3c2f60af08e09d9a
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-07-05 18:33:28 +0500

    Some kind of prototype

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
-
 logging.basicConfig(level=logging.INFO)
 
 if (TOKEN := os.getenv('HANDY_IMAGE_CONVERTER_TOKEN')) is None:
@@ -18,9 +17,43 @@ async def send_welcome(message: types.Message):
     await message.reply("Hi!\n\nI'm HandyImageConverterBot!\n\nPowered by aiogram.")
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+@dp.message_handler(content_types=['document'])
+async def handle_image_as_file(message: types.Message):
+    if image := message.document:
+        match image.mime_type:
+            case 'image/avif':
+                await message.answer(
+                    'Select the output format',
+                    reply_markup=types.ReplyKeyboardMarkup([[types.KeyboardButton('JPEG'),
+                                                             types.KeyboardButton('PNG'),
+                                                             types.KeyboardButton('WEBP')]])
+                )
+            case 'image/jpeg':
+                await message.answer(
+                    'Select the output format',
+                    reply_markup=types.ReplyKeyboardMarkup([[types.KeyboardButton('AVIF'),
+                                                             types.KeyboardButton('PNG'),
+                                                             types.KeyboardButton('WEBP')]])
+                )
+            case 'image/png':
+                await message.answer(
+                    'Select the output format',
+                    reply_markup=types.ReplyKeyboardMarkup([[types.KeyboardButton('AVIF'),
+                                                             types.KeyboardButton('JPEG'),
+                                                             types.KeyboardButton('WEBP')]])
+                )
+            case 'image/webp':
+                await message.answer(
+                    'Select the output format',
+                    reply_markup=types.ReplyKeyboardMarkup([[types.KeyboardButton('AVIF'),
+                                                             types.KeyboardButton('JPEG'),
+                                                             types.KeyboardButton('PNG')]])
+                )
+            case _:
+                await message.answer(
+                    'Error! Unsupported image format!\n\n'
+                    'I support only avif/jpeg/png/webp images at the moment.'
+                )
 
 
 if __name__ == '__main__':
